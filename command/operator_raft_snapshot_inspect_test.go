7737a4e6dcd7a4af49e3326742e6ed5680b837c6
Ellie Sterner <ellie.sterner@hashicorp.com>
2023-10-02 06:42:29 -0500

    added base tests

--- command/operator_raft_snapshot_inspect_test.go
+++ command/operator_raft_snapshot_inspect_test.go
@@ -4,7 +4,7 @@
 package command
 
 import (
-	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/mitchellh/cli"
@@ -24,50 +24,56 @@ func testOperatorRaftSnapshotInspectCommand(tb testing.TB) (*cli.MockUi, *Operat
 func TestOperatorRaftSnapshotInspectCommand_Run(t *testing.T) {
 	t.Parallel()
 
-	// cases := []struct {
-	// 	name string
-	// 	args []string
-	// 	out  string
-	// 	code int
-	// }{
-	// 	{
-	// 		"too_many_args",
-	// 		[]string{"foo"},
-	// 		"Too many arguments",
-	// 		1,
-	// 	},
-	// 	{
-	// 		"default",
-	// 		nil,
-	// 		"Success! Stepped down: ",
-	// 		0,
-	// 	},
-	// }
+	cases := []struct {
+		name string
+		args []string
+		out  string
+		code int
+	}{
+		{
+			"too_many_args",
+			[]string{"./test-fixtures/test.snap", "test"},
+			"Too many arguments",
+			1,
+		},
+		{
+			"default",
+			[]string{"./test-fixtures/test.snap"},
+			"ID           bolt-snapshot",
+			0,
+		},
+		{
+			"kvdetails flag",
+			[]string{"-kvdetails", "./test-fixtures/test.snap"},
+			"Key Name                                          Count",
+			0,
+		},
+	}
 
 	t.Run("validations", func(t *testing.T) {
 		t.Parallel()
 
-		// tc := tc
+		for _, tc := range cases {
+			tc := tc
 
-		t.Run("passing test", func(t *testing.T) {
-			t.Parallel()
+			t.Run(tc.name, func(t *testing.T) {
+				t.Parallel()
 
-			client, closer := testVaultServer(t)
-			defer closer()
+				client, closer := testVaultServer(t)
+				defer closer()
 
-			ui, cmd := testOperatorRaftSnapshotInspectCommand(t)
-			cmd.client = client
+				ui, cmd := testOperatorRaftSnapshotInspectCommand(t)
+				cmd.client = client
+				code := cmd.Run(tc.args)
+				if code != tc.code {
+					t.Errorf("expected %d to be %d", code, tc.code)
+				}
 
-			code := cmd.Run([]string{"complete.snap"})
-			if code != 0 {
-				t.Errorf("expected %d to be %d", code, 0)
-			}
-
-			combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
-			fmt.Println("combined", combined)
-			// if !strings.Contains(combined, tc.out) {
-			// 	t.Errorf("expected %q to contain %q", combined, tc.out)
-			// }
-		})
+				combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
+				if !strings.Contains(combined, tc.out) {
+					t.Errorf("expected %q to contain %q", combined, tc.out)
+				}
+			})
+		}
 	})
 }
